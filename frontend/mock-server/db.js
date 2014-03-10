89b9889ebf29710db9be6601beb828ac8cafe239
Darius <darius@artivest.co>
2014-03-10 10:23:22 -0400

    prepare for timeline feature by mocking up correct date format with randomized ranges

--- frontend/mock-server/db.js
+++ frontend/mock-server/db.js
@@ -21,14 +21,25 @@ exports.run = function() {
         return Math.random()*(max-min+1)+min;
     }
     for(var i=0; i<20; i++) {
+        // start times are in the morning; end times are in afternoon
+        var start = new Date('May 13, 1933');
+        var startHour = randomIntFromInterval(0, 11);
+        var startMinute = randomIntFromInterval(0, 59);
+        start.setHours(startHour, startMinute);
+
+        var end = new Date('May 13, 1933');
+        var endHour = randomIntFromInterval(12, 23);
+        var endMinute = randomIntFromInterval(0, 59);
+        end.setHours(endHour, endMinute);
+
         var newObject = {
             id: (i+1),
             body: "i am a body",
             lat: randomIntFromInterval(50.7, 51),
             lng: randomIntFromInterval(2, -2),
             title: "title " + i,
-            start: "8am",
-            end: "2pm",
+            start: start,
+            end: end,
             message: "",
             political: political[Math.floor(Math.random()*political.length)],
             historical: historical[Math.floor(Math.random()*historical.length)]
