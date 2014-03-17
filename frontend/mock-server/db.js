6af4dfe5e5748d53156d813c926a25e1d1cea379
Darius <darius@artivest.co>
2014-03-17 01:16:32 -0400

    lots in here: removed restangular dependency and just doing plain  for now; got timeline working; got swimlanes working; fixed lots of bugs

--- frontend/mock-server/db.js
+++ frontend/mock-server/db.js
@@ -5,49 +5,65 @@ exports.run = function() {
     var objectTemplate = { 
         "id": 1, 
         "body": "foo",
-        "lat": 51.505,
-        "lng": -0.09,
+        "lat": 48.20817,
+        "lng": 16.37382,
         "title": "first title",
         "start": "8am",
         "end": "2pm",
         "message": "",
-        "political": "Nazi",
         "historical": "Sovereign"
     },
-    political = ['Socialist', 'Nazi', 'Bourgeois', 'Liberal'],
     historical = ['Sovereign', 'Disciplinary', 'Control'];
+    colors = ['B2241A', '1E578F', 'EAFB58']
 
     function randomNumberFromInterval(min,max, isInt) {
         var rand = Math.random()*(max-min+1)+min;
         return isInt ? Math.floor(rand) : rand;
     }
 
-    for(var i=0; i<20; i++) {
+    for(var i=0; i<15; i++) {
         // start times are in the morning; end times are in afternoon
-        var start = new Date('May 13, 1933');
-        var startHour = randomNumberFromInterval(0, 11);
-        var startMinute = randomNumberFromInterval(0, 59);
-        start.setHours(startHour, startMinute);
 
-        var end = new Date('May 13, 1933');
-        var endHour = randomNumberFromInterval(12, 23);
-        var endMinute = randomNumberFromInterval(0, 59);
-        end.setHours(endHour, endMinute);
-
+        var start = randomNumberFromInterval(1, 22, true);
+        var end = randomNumberFromInterval(start+1, start+4, true);
+        if(end>24) end=24;
+        var startPercent = start/24 * 100;
+        var endPercent = end/24 * 100;
+        var historicalIndex = Math.floor(Math.random()*historical.length);
         var newObject = {
             id: (i+1),
             body: "i am a body",
-            lat: randomNumberFromInterval(50.7, 51),
-            lng: randomNumberFromInterval(2, -2),
-            title: "title " + i,
-            start: randomNumberFromInterval(1, 24, true),
-            end: 24,
+            lat: randomNumberFromInterval(47.5, 48),
+            lng: randomNumberFromInterval(16, 17),
+            title: "Actor ID: " + (i+1),
+            start: start,
+            startPercent: startPercent,
+            end: end,
+            endPercent: endPercent,
             message: "",
-            political: political[Math.floor(Math.random()*political.length)],
-            historical: historical[Math.floor(Math.random()*historical.length)]
+            historical: historical[historicalIndex],
+            color: colors[historicalIndex]
         }
         data.actors.push(newObject);
     }
 
+    data.actors.sort(function(a, b) {
+        if(a.start < b.start) {
+            return -1;
+        }
+
+        if(a.start > b.start) {
+            return 1;
+        }
+
+        if(a.end < b.end) {
+            return -1
+        }
+        if(a.end > b.end) {
+            return 1;
+        }
+
+        return 0;
+    });
     return data;
 }
