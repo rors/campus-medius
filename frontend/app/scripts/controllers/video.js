2cf4ca4a7877257343cec4cd464f981b0ae97217
Darius <darius@artivest.co>
2014-04-28 22:22:09 -0400

    get this damn video player working

--- frontend/app/scripts/controllers/video.js
+++ frontend/app/scripts/controllers/video.js
@@ -2,71 +2,34 @@
 
 angular.module('CampusMediusApp')
   .controller('VideoCtrl', function ($scope, $sce) {
-    /* please work */
-    $scope.currentTime = 0;
-    $scope.totalTime = 0;
-    $scope.state = null;
-    $scope.volume = 1;
-    $scope.isCompleted = false;
-    $scope.API = null;
-
-    $scope.onPlayerReady = function(API) {
-      $scope.API = API;
-    };
-
-    $scope.onCompleteVideo = function() {
-      $scope.isCompleted = true;
-    };
-
-    $scope.onUpdateState = function(state) {
-      $scope.state = state;
-    };
-
-    $scope.onUpdateTime = function(currentTime, totalTime) {
-      $scope.currentTime = currentTime;
-      $scope.totalTime = totalTime;
-    };
-
-    $scope.onUpdateVolume = function(newVol) {
-      $scope.volume = newVol;
-    };
-
-    $scope.onUpdateSize = function(width, height) {
-      $scope.config.width = width;
-      $scope.config.height = height;
-    };
-
     $scope.stretchModes = [
-      {label: 'None', value: 'none'},
-      {label: 'Fit', value: 'fit'},
-      {label: 'Fill', value: 'fill'}
-    ];
+            {label: "None", value: "none"},
+            {label: "Fit", value: "fit"},
+            {label: "Fill", value: "fill"}
+        ];
 
-    $scope.config = {
-      width: 200,
-      height: 375,
-      autoHide: false,
-      autoHideTime: 3000,
-      autoPlay: false,
-      responsive: false,
-      stretch: $scope.stretchModes[0],
-      sources: [
-          {src: $sce.trustAsResourceUrl('http://campusmedius.s3.amazonaws.com/assets/fox-newsreel_schoenbrunn-rally.mp4.mp4'), type: 'video/mp4'},
-          {src: $sce.trustAsResourceUrl('http://campusmedius.s3.amazonaws.com/assets/fox-newsreel_schoenbrunn-rally.webmhd.webm'), type: 'video/webm'},
-          {src: $sce.trustAsResourceUrl('http://campusmedius.s3.amazonaws.com/assets/fox-newsreel_schoenbrunn-rally.oggtheora.ogv'), type: 'video/ogg'}
-      ],
-      transclude: true,
-      plugins: {
-          poster: {
-              url: 'images/videogular.png'
-          }
-      }
-    };
-
-    $scope.changeSource = function() {
-      $scope.config.sources = [
-          {src: $sce.trustAsResourceUrl('http://vjs.zencdn.net/v/oceans.mp4'), type: 'video/mp4'},
-          {src: $sce.trustAsResourceUrl('http://vjs.zencdn.net/v/oceans.webm'), type: 'video/webm'}
-      ];
-    };
+        $scope.config = {
+            width: 740,
+            height: 380,
+            autoHide: false,
+            autoPlay: false,
+            responsive: true,
+            stretch: $scope.stretchModes[1],
+            theme: {
+                playIcon: "&#xe000;",
+                pauseIcon: "&#xe001;",
+                volumeLevel3Icon: "&#xe002;",
+                volumeLevel2Icon: "&#xe003;",
+                volumeLevel1Icon: "&#xe004;",
+                volumeLevel0Icon: "&#xe005;",
+                muteIcon: "&#xe006;",
+                enterFullScreenIcon: "&#xe007;",
+                exitFullScreenIcon: "&#xe008;"
+            },
+            plugins: {
+                poster: {
+                    url: "images/videogular.png"
+                }
+            }
+        };
   });
