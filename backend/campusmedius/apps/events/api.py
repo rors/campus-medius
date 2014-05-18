5c91c3117deee581d5aeb4a4b94955b266e210cb
rors <rory@harshmellow.org>
2014-05-18 17:02:49 -0400

    adding lat/lng to API

--- backend/campusmedius/apps/events/api.py
+++ backend/campusmedius/apps/events/api.py
@@ -8,3 +8,9 @@ class EventResource(CORSResource,ModelResource):
         queryset = Event.objects.all()
         resource_name = 'event'
 
+        excludes = [ 'location', ]
+
+    def dehydrate(self, bundle):
+        bundle.data['lat'] = bundle.obj.location.y
+        bundle.data['lng'] = bundle.obj.location.x
+        return bundle
