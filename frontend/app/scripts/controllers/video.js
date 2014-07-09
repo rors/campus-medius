3cb283dec476439efdd322255be1f9b9ba2291ca
rors <rory@harshmellow.org>
2014-07-09 13:51:31 -0400

    hiding vimeo title screen junk

--- frontend/app/scripts/controllers/video.js
+++ frontend/app/scripts/controllers/video.js
@@ -3,6 +3,6 @@
 angular.module('CampusMediusApp')
   .controller('VideoCtrl', ['$scope', '$sce', function ($scope, $sce) {
         $scope.getIframeSrc = function(id) {
-            return  $sce.trustAsResourceUrl('http://player.vimeo.com/video/' + id);
+            return  $sce.trustAsResourceUrl('http://player.vimeo.com/video/' + id + '?title=0&byline=0&portrait=0');
         }
   }]);
